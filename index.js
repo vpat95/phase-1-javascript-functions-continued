2a1c6e18139358c3d574debe70cabe5f6abbc046
Vivek Patel <vpatel360@gmail.com>
2023-06-15 00:01:13 -0400

    commit tree fb84eb4b1fa5af1bb4f5c5285715e228a4d6e7f2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun (activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+ const mondayWork = function (activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+ }
+
+ const wrapAdjective = function(flair="*"){
+    return function(adjective="special"){
+        return `You are ${flair + adjective + flair}!`
+    }
+ }
